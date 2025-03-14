5f9a132026eacbdf4b37d8433df6394992ca3bc1
Vinicius <vivinhovivo10@gmail.com>
2025-03-14 01:49:15 -0300

    Adicionando configurações de banco de dados no application.properties

--- src/main/java/me/dio/primeira/Application.java
+++ src/main/java/me/dio/primeira/Application.java
@@ -3,6 +3,9 @@ package me.dio.primeira;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default server URL")})
 @SpringBootApplication
 public class Application {
 
